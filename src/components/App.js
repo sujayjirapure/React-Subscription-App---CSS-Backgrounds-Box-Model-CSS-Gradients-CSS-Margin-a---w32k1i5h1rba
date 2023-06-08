60648ea25b22305a72da21e8df5c6bf8fdb975bd
Sujay jirapure <110679753+sujayjirapure@users.noreply.github.com>
2023-06-08 14:20:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,51 @@ import React from "react";
 import "../styles/App.css";
 const App = () => {
   return (
-     <div className="container">
-      <div className="community box">
-        <h2>Community</h2>
-        <p>Free</p>
-      </div>
-      <div className="card box">
-        <h2>Starter</h2>
-        <p>$9.99/month</p>
-      </div>
-      <div className="why-us box">
-        <h2>Pro</h2>
-        <p>$19.99/month</p>
-        <p>Unlock all features, including:</p>
-        <ul>
-          <li>Unlimited access to all courses</li>
-          <li>Expert support from our team</li>
-          <li>Advanced analytics and reporting</li>
-        </ul>
-        <button>Subscribe now</button>
-      </div>
-    </div>
+    <div id="main">
+			<section class="card-section">
+              <div class="container">
+                <div class="community">
+                  <h2 class="community-header">Join our community</h2>
+                  <h3 class="community-sub-header">
+                    30-day, hassle-free monkey back gurarantee
+                  </h3>
+                  <p class="community-text">
+                    Gain access to our full library of tutorials along with expert
+                    code reviews. Perfect for any developers who are serious about
+                    honing their skills.
+                  </p>
+                </div>
+                <div class="card-details">
+                  <div class="card">
+                    <p class="card-header">Monthly Subscription</p>
+                    <div class="price">
+                      <span>$29</span>
+                      <p>Per Month</p>
+                    </div>
+                    <p class="card-details-text">
+                      Full access for less than $1 a day.
+                    </p>
+                    <a href="#" class="btn btn-card">
+                      Sign Up
+                    </a>
+                  </div>
+                  <div class="why-us">
+                    <h4 class="why-us-header">Why Us</h4>
+                    <div class="why-us-lists">
+                      <ul class="why-us-items">
+                        <li class="why-us-item">Tutorials by industry experts</li>
+                        <li class="why-us-item">Peer & expert code review</li>
+                        <li class="why-us-item">Coding exercises</li>
+                        <li class="why-us-item">Access yo our GitHub repos</li>
+                        <li class="why-us-item">Community forum</li>
+                        <li class="why-us-item">Flashcard decks</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+		</div>
   );
 };
 
